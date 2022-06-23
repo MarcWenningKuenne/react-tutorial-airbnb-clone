e2cc9709cecdd1a6602569b4ee64d4ada2633240
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-23 20:48:58 +0200

    21 Map data into components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React from "react";
 import "./style.css"
+import data from "./data.js";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 
 /*
-Challenge: Pass props to the Card component and display that data
+Challenge:
 
-- img ("katie-zaferes.png")
-- rating ("5.0")
-- reviewCount (6)
-- country (Whatever you want)
-- title ("Life Lessons with Katie Zaferes")
-- price (136)
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
 
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
 */
 
 function App() {
+  const dataJSX = data.map((x) => {
+    return <Card 
+      img = {x.coverImg}
+      rating = {x.stats.rating}
+      reviewCount = {x.stats.reviewCount}
+      country = {x.location}
+      title = {x.title}
+      price = {x.price}
+    />
+  })
+  
+  console.log(dataJSX)
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card 
-        img = "card-image.png"
-        rating = "5.0"
-        reviewCount = {6}
-        country = "USA"
-        title = "Life Lessons with Katie Zaferes"
-        price = {136}
-      />
+      {dataJSX}
     </div>
   );
 }
